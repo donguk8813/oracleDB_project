775cd9149821b461d8107470d78008f3628265fe
donguk8813 <donguk8813@gmail.com>
2021-02-18 15:37:54 +0900

    DbColumns 수정

--- src/main/java/com/example/demo/dong/domain/DbColumns.java
+++ src/main/java/com/example/demo/dong/domain/DbColumns.java
@@ -4,12 +4,6 @@ import lombok.Data;
 
 @Data
 public class DbColumns {
-	/*
-	 * private String owner; private String tableName;
-	 * 
-	 * private String indexName; private String indexType; private String tableOwner; private String tableType; private
-	 * String uniqueness; private String compression;
-	 */
 
 	private String tablespaceName;
 	private String segmentType;
